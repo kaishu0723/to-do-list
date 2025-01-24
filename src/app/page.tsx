0804bedb26c4cbd10e331b0a09222c807e53dbe7
kaishu <m.kaishu0723@gmailcom>
2025-01-24 12:05:44 +0900

    prototype complite

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,88 @@
-import Image from "next/image";
+"use client";
+import { useState, useEffect} from "react";
+
+type Task={
+  id:number;
+  title:string;
+  completed:boolean;
+};
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [tasks,setTasks]=useState<Task[]>([]);
+  const [newTask,setNewTask]=useState("");
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  useEffect(()=>{
+    const data=localStorage.getItem("tasks");
+    setTasks(JSON.parse(data));
+  },[]);
+  useEffect(()=>{
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+  },[tasks]);
+
+  const addTask=()=>{
+    //trim()関数は文字列の前後の空白を削除するので、空文字（または空白文字のみ）の場合はreturnで関数を終了
+    if(!newTask.trim()) return;
+    const newTaskOBJ:Task={
+      id:tasks.length+1,
+      title:newTask,
+      completed:false
+    };
+    //prevは現在の状態を指す
+    //setTasksの中身をコピーして新しいオブジェクトを追加している。
+    setTasks((prev)=>[...prev,newTaskOBJ]);
+
+    setNewTask("");
+  }
+
+  const deleteTask=()=>{
+    //filter((task)=>true)
+    //trueなら残る、falseなら落ちる
+    setTasks((prevTasks)=>prevTasks.filter((task)=>!task.completed));
+  }
+
+  const toggleTask=(id:number)=>{
+    setTasks((prevTasks)=>
+      prevTasks.map((task)=>
+        //...はスプレット構文でオブジェクトtaskをコピーする（不変性を維持するため）
+        task.id===id ? {...task,completed:!task.completed}:task 
+      )
+    );
+  };
+  
+  
+  return (
+    //input > onChangeは入力毎に指定の関数を実行する
+    //(e)はイベントオブジェクト、inputの状態を取得できる。
+    //この場合、入力された文字をsetNewTasksの引数に当てている。
+    //flex justify-center items-center
+    <div>
+      <h1 className="text-4xl my-10">Tasks</h1>
+      <form action={addTask} className="my-8">
+        <input
+          className="border border-gray-400 rounded-lg px-4 h-10"
+          type="text"
+          value={newTask}
+          onChange={(e)=>setNewTask(e.target.value)}
+          placeholder="New Task"
+        />
+        <button onClick={addTask} className="w-16 h-10 rounded-lg mx-4 bg-black text-white">Add</button>
+        <button onClick={deleteTask} className="w-16 h-10 rounded-lg bg-white text-black border-2">Delete</button>
+      </form>
+      <ul>
+        {
+          tasks.map((task)=>
+            <li key={task.id} className="bg-white/80 rounded-lg shadow-lg w-80 h-20 mx-auto my-4 flex justify-center items-center">
+              <label className="text-2xl"><input
+                  className="mr-4 size-4"
+                  type="checkbox"
+                  checked={task.completed}
+                  onChange={()=>toggleTask(task.id)}
+                />{task.title}
+              </label>
+            </li>
+          )
+        }
+      </ul>
     </div>
   );
 }
